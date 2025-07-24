aa4ac1d1f8933b4a8ba3ce5b9f0527770fc6007d
sten-tpr <stenfold@gmail.com>
2025-07-24 20:23:54 +0200

    [FIX] bypass security and add of data for h2

--- bank-account-backend/src/main/java/com/sg/kata/model/Transaction.java
+++ bank-account-backend/src/main/java/com/sg/kata/model/Transaction.java
@@ -1,13 +1,11 @@
 package com.sg.kata.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "transactions")
 public class Transaction {
 
     @Id
---
+++ bank-account-backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO transactions (date, amount, balance) VALUES ('2024-01-01', 100, 100);
+INSERT INTO transactions (date, amount, balance) VALUES ('2024-01-03', -20, 80);
+INSERT INTO transactions (date, amount, balance) VALUES ('2024-01-04', 50, 130);
